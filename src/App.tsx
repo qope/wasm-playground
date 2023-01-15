f59a5ffdf756f23b4b58efdbbbe9fcf0021842f5
nuno <41233686+qope@users.noreply.github.com>
2023-01-16 05:33:53 +0900

    add performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import init, { ip_get } from "wasm-lib";
 
 function App() {
+  const [result, setResult] = useState("");
+  const [time, setTime] = useState(0);
+
   const getIP = () => {
     (async () => {
+      setResult("");
+      setTime(0);
+      const start = performance.now();
       await init();
       const ip = await ip_get();
       console.log(ip);
+      const end = performance.now();
+      setResult(ip);
+      setTime(Math.round(end - start));
     })();
   };
 
@@ -15,7 +24,10 @@ function App() {
     <div className="App">
       <p>
         <button onClick={getIP}> get ip</button>
+        <button onClick={getIP}> get ip</button>
       </p>
+      <p>{result}</p>
+      <p>time: {time} ms</p>
     </div>
   );
 }
