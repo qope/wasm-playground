0c512b09145e73d442a4d916e6fe8abbcec86015
nuno <41233686+qope@users.noreply.github.com>
2023-01-16 05:20:32 +0900

    add ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import init, { ip_get } from "wasm-lib";
 
 function App() {
+  const getIP = () => {
+    (async () => {
+      await init();
+      const ip = await ip_get();
+      console.log(ip);
+    })();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <button onClick={getIP}> get ip</button>
+      </p>
     </div>
   );
 }
